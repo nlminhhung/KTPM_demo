e717f64eac4a9866e6132eb3e6e8ca174350ff1a
nlminhhung <nlmhung2511@gmail.com>
2024-03-11 09:57:46 +0700

    resolve feature branch

--- helloworld.java
+++ helloworld.java
@@ -1,9 +1,6 @@
 public class helloworld {
     public static void main(String[] args) {
-        int a = 0;
-        if (a != 0){
-            System.out.println("khac 0!");
-        }
+        System.out.println("khac 0!");
 
     }
 }
