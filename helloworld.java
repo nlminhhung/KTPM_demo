d36319f9d7bbe2c13de27684ab547220c742be4c
nlminhhung <nlmhung2511@gmail.com>
2024-03-11 09:46:10 +0700

    resolve conflict

--- helloworld.java
+++ helloworld.java
@@ -1,9 +1,5 @@
 public class helloworld {
     public static void main(String[] args) {
-        int b = 0;
-        if (b == 0)
-        {
-            System.out.println("Bang 0!");
-        }
+    
     }
 }
